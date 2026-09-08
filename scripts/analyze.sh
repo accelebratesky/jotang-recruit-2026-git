1cbc3cdc1518c3361ecb96d55d480f5cee519e51
accelebrate <qingban88@qq.com>
2026-09-08 18:33:38 +0800

    finish all

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
+if [[ $# -ne 1 ]]; then
+echo "Usage: ./scripts/analyze.sh FILE"
+exit 1
+fi
 
-# Task 07: complete this script.
-# Usage: ./scripts/analyze.sh FILE
+LOG_FILE="$1"
 
-# TODO: validate arguments
-# TODO: validate file existence
-# TODO: print:
-# Total ERROR: <number>
-# Top Code: <code>
+if [[ ! -f "$LOG_FILE" ]]; then
+    echo "Error: File $LOG_FILE does not exist"
+    exit 1
+fi
+
+total_error=$(grep -c "ERROR" "$LOG_FILE")
+top_code=$(awk '{print $5}' "$LOG_FILE" | awk -F '=' '{print $2}' | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}')
+echo "Total ERROR: $total_error"
+echo "Top Code: $top_code"
+
+exit 0
